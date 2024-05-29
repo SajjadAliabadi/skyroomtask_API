12c079d85d1993e09ac144b92cac708a429d4f6c
sajjad <sajjad@gmail.com>
2024-05-29 18:50:06 +0330

    create layout (header and footer) and index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    include "layout/header.php";
+?>
+
+    <div class="d-flex justify-content-center mt-5">
+        <div class="col-4">
+            <div class="card p-3">
+                <img src="./public/images/usermange.jfif" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title mb-5"> User management system</h5>
+                </div>
+                <a href="web" class="btn btn-primary btn-block ">WEB</a>
+                <a href="api" class="btn btn-secondary  btn-block mt-1">API</a>
+            </div>
+        </div>
+    </div>
+
+
+<?php
+    include "layout/footer.php";
+?>
